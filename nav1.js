782a292e2c437a001e15c0ffbc218da2fc524bf6
Hamzazeh <147081037+Hamzazeh@users.noreply.github.com>
2025-01-21 20:32:29 +0100

    Add files via upload

---
+++ nav1.js
@@ -0,0 +1,12 @@
+
+
+
+function show_navbar(){
+	document.querySelector("#ycor").style.display = "flex"
+}
+function hidden_navbar(){
+	document.querySelector("#ycor").style.display = "none"
+}
+
+
+
