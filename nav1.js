24f1d9a35d0790e4ea59bcd0a830c926fbc27200
Hamzazeh <147081037+Hamzazeh@users.noreply.github.com>
2025-01-21 20:53:14 +0100

    Update nav1.js

--- nav1.js
+++ nav1.js
@@ -3,9 +3,11 @@
 
 function show_navbar(){
 	document.querySelector("#ycor").style.display = "flex"
+	document.querySelector(".overlay").style.display = "block"
 }
 function hidden_navbar(){
 	document.querySelector("#ycor").style.display = "none"
+	document.querySelector(".overlay").style.display = "none"
 }
 
 
